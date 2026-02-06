7db6b787f7e73eb19ba31bfed389c34ffcd63c4e
Strahinja Paunkovic <paunkovicwork@gmail.com>
2026-02-06 19:35:04 +0100

    Add .env example and reading from .env file

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,8 @@
 from app.database_connection import DatabaseConnection
+from app.env_loader import require_env
 
-DATABASE_URL = "sqlite+aiosqlite:///database.db"
+
+DATABASE_URL = require_env("DATABASE_URL")
 
 # SINGLE shared db instance for whole app
 db = DatabaseConnection(DATABASE_URL, echo=True)
