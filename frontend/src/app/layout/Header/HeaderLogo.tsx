27c867223eff6921644d3234f275ab855c2d7864
Strahinja Paunkovic <paunkovicwork@gmail.com>
2026-06-22 20:30:04 +0200

    Remove inline css from header components

--- frontend/src/app/layout/Header/HeaderLogo.tsx
+++ frontend/src/app/layout/Header/HeaderLogo.tsx
@@ -2,15 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function HeaderLogo() {
   return (
-    <Link
-      to="/"
-      style={{
-        color: "#fff",
-        fontSize: 24,
-        fontWeight: 800,
-        textDecoration: "none",
-      }}
-    >
+    <Link to="/" className="header__logo">
       Booking.com
     </Link>
   );
