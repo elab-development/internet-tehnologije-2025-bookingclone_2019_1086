c037aef5cf3fc18d47fe47e8baaca76fd036b8ef
Strahinja Paunkovic <paunkovicwork@gmail.com>
2026-06-23 09:44:57 +0200

    Refactor map

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./i18n/i18n";
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
